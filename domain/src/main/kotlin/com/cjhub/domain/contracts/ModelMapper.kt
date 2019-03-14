0a62b21787cba6e714404bca98a3257a189129d7
Chanon Jenakom <chanonjenakom@gmail.com>
2019-03-14 11:34:29 +0700

    Rename 'Mapper' to 'ModelMapper'

--- domain/src/main/kotlin/com/cjhub/domain/contracts/ModelMapper.kt
+++ domain/src/main/kotlin/com/cjhub/domain/contracts/ModelMapper.kt
@@ -3,7 +3,7 @@ package com.cjhub.domain.contracts
 /**
  * Mapper used to map data entities to domain models and vice versa.
  */
-interface Mapper<E, M> {
+interface ModelMapper<E, M> {
 
     fun toModel(from: E): M
 
