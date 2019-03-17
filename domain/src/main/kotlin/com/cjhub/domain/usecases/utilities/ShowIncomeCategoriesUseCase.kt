3f1012b1ef203d5c0b0dce155c7151af09ec8533
Chanon Jenakom <chanonjenakom@gmail.com>
2019-03-17 13:34:53 +0700

    Implement 'Show Income Categories' use case

--- domain/src/main/kotlin/com/cjhub/domain/usecases/utilities/ShowIncomeCategoriesUseCase.kt
+++ domain/src/main/kotlin/com/cjhub/domain/usecases/utilities/ShowIncomeCategoriesUseCase.kt
@@ -4,11 +4,12 @@ import io.reactivex.Single
 
 import com.cjhub.domain.contracts.repositories.CategoryRepository
 import com.cjhub.domain.models.Category
+import com.cjhub.domain.models.Type
 
 /**
  * Show all the income categories stored in the database.
  */
 class ShowIncomeCategoriesUseCase(private val categoryRepository: CategoryRepository) {
 
-    fun show(): Single<List<Category>> = TODO()
+    fun show(): Single<List<Category>> = categoryRepository.getAllByType(Type.INCOME)
 }
