999dafc2b523f095d34057eef727f4cdb5403e68
Chanon Jenakom <chanonjenakom@gmail.com>
2019-03-17 13:30:53 +0700

    Create unit test for 'Show Income Categories' use case

--- domain/src/test/kotlin/com/cjhub/domain/usecases/utilities/ShowCategoriesFeature.kt
+++ domain/src/test/kotlin/com/cjhub/domain/usecases/utilities/ShowCategoriesFeature.kt
@@ -1,10 +1,38 @@
 package com.cjhub.domain.usecases.utilities
 
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+
+import com.cjhub.domain.contracts.repositories.CategoryRepository
+import com.cjhub.domain.models.Type
 
 /**
  * Spek tests for showing different types of categories.
  */
 object ShowCategoriesFeature : Spek({
 
+    Feature("Show Categories") {
+
+        val categoryRepository by memoized { mock<CategoryRepository>() }
+
+        val showIncomeCategoriesUseCase by memoized {
+            ShowIncomeCategoriesUseCase(categoryRepository)
+        }
+        val showExpenseCategoriesUseCase by memoized {
+            ShowExpenseCategoriesUseCase(categoryRepository)
+        }
+
+        Scenario("The user wants to request for the income categories") {
+
+            When("The user requests for the income categories") {
+                showIncomeCategoriesUseCase.show()
+            }
+            Then("the system should get the list of income categories") {
+                verify(categoryRepository).getAllByType(Type.INCOME)
+            }
+        }
+    }
 })
