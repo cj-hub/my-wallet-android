216a80bda6c9304c1530e5741a104e7dbcba494b
Chanon Jenakom <chanonjenakom@gmail.com>
2019-03-17 13:35:53 +0700

    Implement 'Show Expense Categories' use case

--- domain/src/main/kotlin/com/cjhub/domain/usecases/utilities/ShowExpenseCategoriesUseCase.kt
+++ domain/src/main/kotlin/com/cjhub/domain/usecases/utilities/ShowExpenseCategoriesUseCase.kt
@@ -4,11 +4,12 @@ import io.reactivex.Single
 
 import com.cjhub.domain.contracts.repositories.CategoryRepository
 import com.cjhub.domain.models.Category
+import com.cjhub.domain.models.Type
 
 /**
  * Show all the expense categories stored in the database.
  */
 class ShowExpenseCategoriesUseCase(private val categoryRepository: CategoryRepository) {
 
-    fun show(): Single<List<Category>> = TODO()
+    fun show(): Single<List<Category>> = categoryRepository.getAllByType(Type.EXPENSE)
 }
