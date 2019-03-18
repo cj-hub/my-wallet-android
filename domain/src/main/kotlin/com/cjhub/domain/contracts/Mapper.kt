b82b8399ec928032594376d865889ba1cbaf5a6e
Chanon Jenakom <chanonjenakom@gmail.com>
2019-03-18 12:47:01 +0700

    Update 'Mapper' contract

--- domain/src/main/kotlin/com/cjhub/domain/contracts/Mapper.kt
+++ domain/src/main/kotlin/com/cjhub/domain/contracts/Mapper.kt
@@ -3,7 +3,7 @@ package com.cjhub.domain.contracts
 /**
  * Mapper used to map data entities to domain models and vice versa.
  */
-interface ModelMapper<E, M> {
+interface Mapper<E, M> {
 
     fun toModel(from: E): M
 
