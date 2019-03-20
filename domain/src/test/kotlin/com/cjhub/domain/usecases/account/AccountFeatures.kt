86fc6894d2aa9dd700150c3bfb25f80a823646c4
Chanon Jenakom <chanonjenakom@gmail.com>
2019-03-21 00:08:17 +0700

    Create unit test for 'Show Accounts' use case

--- domain/src/test/kotlin/com/cjhub/domain/usecases/account/AccountFeatures.kt
+++ domain/src/test/kotlin/com/cjhub/domain/usecases/account/AccountFeatures.kt
@@ -1,10 +1,32 @@
 package com.cjhub.domain.usecases.account
 
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+
+import com.cjhub.domain.contracts.repositories.AccountRepository
 
 /**
  * Spek tests for various account use cases.
  */
 object AccountFeatures : Spek({
 
+    Feature("Show Accounts") {
+
+        val accountRepository by memoized { mock<AccountRepository>() }
+
+        val showAccountsUseCase by memoized { ShowAccountsUseCase(accountRepository) }
+
+        Scenario("The user wants to request for the accounts") {
+
+            When("the user requests for the accounts") {
+                showAccountsUseCase.show()
+            }
+            Then("the system should get the list of accounts") {
+                verify(accountRepository).getAll()
+            }
+        }
+    }
 })
