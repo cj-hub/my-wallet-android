648af823bc87e668c89cf6078a64a75509745f93
Chanon Jenakom <chanonjenakom@gmail.com>
2019-03-25 12:05:23 +0700

    Implement 'Delete Category' use case

--- domain/src/main/kotlin/com/cjhub/domain/usecases/settings/DeleteCategoryUseCase.kt
+++ domain/src/main/kotlin/com/cjhub/domain/usecases/settings/DeleteCategoryUseCase.kt
@@ -6,6 +6,7 @@ import com.cjhub.domain.contracts.repositories.CategoryRepository
 import com.cjhub.domain.contracts.repositories.TransactionRepository
 import com.cjhub.domain.models.Category
 import com.cjhub.domain.models.Transaction
+import com.cjhub.domain.models.Type
 
 /**
  * Delete an existing category from the database.
@@ -20,5 +21,19 @@ class DeleteCategoryUseCase(
         relatedTransactions: List<Transaction>,
         otherIncome: Category,
         otherExpense: Category
-    ): Completable = TODO()
+    ): Completable {
+        val updatedCategory = if (category.type == Type.INCOME) {
+            otherIncome.copy(total = otherIncome.total + category.total)
+        } else {
+            otherExpense.copy(total = otherExpense.total + category.total)
+        }
+
+        return categoryRepository.insertOrUpdate(updatedCategory)
+                .andThen(Completable.merge(
+                    relatedTransactions.map { transaction ->
+                        transactionRepository.insertOrUpdate(transaction.copy(category = updatedCategory))
+                    }
+                ))
+                .andThen(categoryRepository.delete(category))
+    }
 }
