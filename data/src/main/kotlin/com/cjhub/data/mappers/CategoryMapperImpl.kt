cb00f80606d3602020d0c2d97c460b9310142a46
Chanon Jenakom <chanonjenakom@gmail.com>
2019-03-28 11:07:42 +0700

    Implement Category mapper

--- data/src/main/kotlin/com/cjhub/data/mappers/CategoryMapperImpl.kt
+++ data/src/main/kotlin/com/cjhub/data/mappers/CategoryMapperImpl.kt
@@ -2,6 +2,7 @@ package com.cjhub.data.mappers
 
 import com.cjhub.domain.contracts.Mapper
 import com.cjhub.domain.models.Category
+import com.cjhub.domain.models.Type
 
 import com.cjhub.data.entities.CategoryEntity
 
@@ -10,7 +11,28 @@ import com.cjhub.data.entities.CategoryEntity
  */
 class CategoryMapperImpl : Mapper<CategoryEntity, Category> {
 
-    override fun toModel(from: CategoryEntity): Category = TODO()
+    override fun toModel(from: CategoryEntity): Category {
+        return Category(
+            from.id,
+            from.name,
+            when (from.type) {
+                "Income" -> Type.INCOME
+                "Expense" -> Type.EXPENSE
+                "Transfer" -> Type.TRANSFER
+                else -> Type.NONE
+            },
+            from.total
+        )
+    }
 
-    override fun toEntity(from: Category): CategoryEntity = TODO()
+    override fun toEntity(from: Category): CategoryEntity {
+        return CategoryEntity(
+            from.id,
+            from.name,
+            from.type.toString(),
+            from.total,
+            System.currentTimeMillis()
+
+        )
+    }
 }
